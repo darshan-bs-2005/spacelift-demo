7b5294b4eca6b478b1644da89a2040ab36c3d507
Darshan B suttakoti <darshanbs189@gmail.com>
2026-09-13 20:55:48 +0530

    Update main.tf

--- tf-ansible-stack-dependencies/tf/main.tf
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -19,7 +19,8 @@ data "aws_ami" "ubuntu" {
     name   = "architecture"
     values = ["x86_64"]
   }
-  owners = ["099720109477"] #canonical
+
+  owners = ["099720109477"] # Canonical
 }
 
 locals {
@@ -28,14 +29,17 @@ locals {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t3.micro"
     }
+
     instance2 = {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t3.micro"
     }
+
     instance3 = {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t3.micro"
     }
+
     instance4 = {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t3.micro"
@@ -54,6 +58,12 @@ resource "aws_instance" "this" {
   instance_type               = each.value.instance_type
   key_name                    = aws_key_pair.aws.key_name
   associate_public_ip_address = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y python3.9
+  EOF
 
   tags = {
     Name = each.key
